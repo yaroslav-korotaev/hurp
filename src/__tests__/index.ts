25243f346a3432e0a62107e6cf2a8eb677be30bd
Yaroslav Korotaev <yaroslav.korotaev@gmail.com>
2018-11-11 02:07:06 +0300

    update deps and fix tests

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,3 +1,7 @@
+// Importing 'jest-extended' explicitly for VS Code IntelliSense support until the better solution
+// is found
+// tslint:disable-next-line:no-import-side-effect
+import 'jest-extended';
 import foo from '../index';
 
 describe('foo', () => {
